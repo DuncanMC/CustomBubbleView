01f5885f6b4433bb5b90124f7bd58d10fd357ce9
Duncan Champney <DChampney@WareTo.com>
2023-11-21 11:48:23 -0500

    changes committed.

---
+++ CustomBubbleView/CustomBubbleView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomBubbleView.swift
+//  CustomBubbleView
+//
+//  Created by Duncan Champney on 11/21/23.
+//
+
+import UIKit
+
+class CustomBubbleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
